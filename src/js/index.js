d104c5c95e9920a138036eca3ed13cc3ab1a4a97
李杰 <lijie8@jd.com>
2018-05-09 10:57:28 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
+
 import WebAR from './webar';
 import VConsole from './vconsole.min';
+
 new VConsole();
 
 const ua = navigator.userAgent.toLowerCase(),
@@ -7,104 +9,110 @@ const ua = navigator.userAgent.toLowerCase(),
     isIphone = /(iPhone|iPad|iPod|iOS)/i.test(ua),
     isWeChat = /MicroMessenger/i.test(ua);
 
+const winHeight = window.innerHeight;
 const webAR = new WebAR();
 const startPanel = $(".openPanel");
 const scanPanel = $(".scan-panel");
-const openCamera = $("#openCamera");
+const btnOpenCamera = $("#openCamera");
 const video = $('#video')[0];
 const scanButon = $(".scan-button");
 
+//是否支持
+let supportVideo = true;
 
+//点击开启
+btnOpenCamera.on('click', function() {
+  if(supportVideo){
+    startPanel.hide();
+    scanPanel.show();
+  }else{
+    alter('直接观看视频')
+  }
+});
 
 /*
  * 支持打开摄像头
  */
-function success(){
-  //显示启动页
-  startPanel.show();
+function success() {
+    //显示启动页
+    startPanel.show();
+    console.log('xx');
 
 }
 /*
  * 不支持开启摄像头
  */
-function fail(){
-    
+function fail() {
+
     //如果是iphone和weiChat 显示引导页
     if (isIphone && isWeChat) {
-      $(".ioswxPanel").show();
-      return;
+        $(".ioswxPanel").show();
+        return;
     }
+    //不支持
+    supportVideo = false;
 
-    //
 }
-/*
- * 摄像头开启成功
- */
-function openSuccess(){
-  //
-  startPanel.hide();
-  scanPanel.show();
-}
+
 //识别
-function scan(){
+function scan() {
 
 }
-
 
 let deviceId; //指定调用设备ID
 // 列出视频设备
-webAR.listCamera()
-    .then((videoDevice) => {
-        console.log(videoDevice);
-        //测了一些手机，android后置摄像头应该是数组的最后一个，苹果是第一个
-        if(isAndroid){
-        	console.log('android');
-        	deviceId = videoDevice[videoDevice.length -1].deviceId;
-        }else if(isIphone){
-        	deviceId = videoDevice[0].deviceId;
-        }
-        success();
+webAR.listCamera().then((videoDevice) => {
+    console.log(videoDevice);
+    //测了一些手机，android后置摄像头应该是数组的最后一个，苹果是第一个
 
-    })
-    .catch((err) => {
-        console.log(err);
-        fail();
-        //
-    });
+    if (isAndroid) {
+        deviceId = videoDevice[videoDevice.length - 1].deviceId;
+    } else if (isIphone) {
+        deviceId = videoDevice[0].deviceId;
+    }
 
+    success();
+    //检查成功直接开启
+    openCamera();
 
-openCamera.on('click', function() {
-    webAR.openCamera(video, deviceId)
-        .then((msg) => {
-            // 打开摄像头成功
-            // 将视频铺满全屏(简单处理)
-            window.setTimeout(() => {
-                let videoWidth = video.offsetWidth;
-                let videoHeight = video.offsetHeight;
+}).catch((err) => {
 
-                if (window.innerWidth < window.innerHeight) {
-                    // 竖屏
-                    if (videoHeight < window.innerHeight) {
-                        video.setAttribute('height', window.innerHeight.toString() + 'px');
-                    }
-                } else {
-                    // 横屏
-                    if (videoWidth < window.innerWidth) {
-                        video.setAttribute('width', window.innerWidth.toString() + 'px');
-                    }
-                }
-            }, 500);
-
-            openSuccess();
-
-
-        })
-        .catch((err) => {
-            alert('打开视频设备失败');
-        });
-
+    fail();
+    //
 });
+function openCamera(){
+
+  webAR.openCamera(video, deviceId).then((msg) => {
+      // 打开摄像头成功
+      // 将视频铺满全屏(简单处理)
+      window.setTimeout(() => {
+          let videoWidth = video.offsetWidth;
+          let videoHeight = video.offsetHeight;
+
+          if (window.innerWidth < window.innerHeight) {
+              // 竖屏
+              if (videoHeight < window.innerHeight) {
+                  video.setAttribute('height', window.innerHeight.toString() + 'px');
+              }
+          } else {
+              // 横屏
+              if (videoWidth < window.innerWidth) {
+                  video.setAttribute('width', window.innerWidth.toString() + 'px');
+              }
+          }
+
+      }, 500);
+
+      var offsetHeight = winHeight * 0.16;
+      $(".scan-boder").css("border-top", "solid " + offsetHeight + "px rgba(0,0,0,.3)");
+
+  }).catch((err) => {
+      alert('打开视频设备失败');
+  });
+}
+
+
 //对准完成按钮
-scanButon.on('click',function(){
-  scan();
+scanButon.on('click', function() {
+    scan();
 })
---
+++ src/js/preload.js
@@ -0,0 +1,38 @@
+window.createjs = {}
+
+require('./preloadjs.min.js');
+require('./soundjs.min.js');
+
+let $preload = $("#preload");
+let $progress = $("#progress");
+
+let preload = new createjs.LoadQueue(false);
+
+
+preload.on("fileload", handleFileLoad);
+preload.on("complete", handleComplete, this);
+preload.on("progress", handleOverallProgress, this);
+preload.installPlugin(createjs.Sound);
+preload.loadManifest([
+	{src:"../img/scan.gif"},
+	{src:"../img/bg.jpg"},
+	{src:"../img/button.png"},
+    // { id: "music", src: "http://storage.jd.com/web-static/735ef47dd77d0c8fa69b3370f59aef81.mp4"},
+]);
+
+function handleFileLoad(event) {
+
+}
+
+function handleComplete() {
+  
+     $preload.hide();
+    // $container.show();
+}
+
+function handleOverallProgress() {
+    //console.log(this);
+    var progress = Math.floor(preload.progress * 100);
+    $progress.html(progress +'%');
+
+}
